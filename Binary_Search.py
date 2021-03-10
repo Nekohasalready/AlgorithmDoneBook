70a9cbd4215cc7c6f37716337e57fc961d28c4e4
Yao Li <18755354082@163.com>
2021-03-10 13:30:26 +0800

    Create Binary_Search.py

---
+++ Binary_Search.py
@@ -0,0 +1,26 @@
+#Binary Search 二分查找
+#执行用时：36 ms, 在所有 Python3 提交中击败了99.91% 的用户
+#内存消耗：15.7 MB, 在所有 Python3 提交中击败了85.86% 的用户
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        listlenth=len(nums)
+        
+        '''定义二分范围的起点和终点'''
+        start=0
+        end=listlenth-1
+        targetindex=-1 #如果最后没有找到目标值，那么返回-1
+        
+        '''开始循环，直到左边界>右边界为止'''
+        while(start<=end):
+            midindex=start+int((end-start)/2) #计算中点位置
+            if nums[midindex]<target: #当前值比目标值小，那么应该在右侧继续搜寻
+                start=midindex+1
+            elif nums[midindex]>target: #当前值比目标值大，那么应该在左侧继续搜寻
+                end=midindex-1
+            else:
+                targetindex=midindex #找到目标值，返回其位置
+                break
+        return targetindex
+      
+      
